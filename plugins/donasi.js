d78b3b50b79f9b6a1ff538befb7ed8cea52c154a
Rafiajiya <94803405+Rafiajiya@users.noreply.github.com>
2022-04-12 12:26:12 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
 *Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
 ╔═══════════════════
-║ _*DONASI UNTUK*_  𝙆𝙧𝙞𝙯𝙮𝙣 ×͜×
+║ _*DONASI UNTUK*_  Rafi Aji×͜×
 ╠═══════════════════
 ║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
-║│➸ *DANA* : 081360482998
-║│➸ *PULSA*: 081360482998
+║│➸ *DANA* : 088233481992
+║│➸ *PULSA*: 088233481992
 ║│➸ *GOPAY*: Lagi Gak pakai
 ║╰──────────────────
 ╰═══════════════════
